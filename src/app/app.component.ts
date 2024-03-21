4293847dbadf38048741a7eb304cdd4c61235c88
kabila-datasirpi <kabila.m@datasirpi.com>
2024-03-21 09:44:17 +0530

    fontend chat added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Pusher from 'pusher-js';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'routing';
+export class AppComponent implements OnInit {
+  username = 'username';
+  message = '';
+  messages = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('0bd23784539ebb183e34', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', data => {
+      this.messages.push(data);
+    });
+  }
+
+  submit(): void {
+    this.http.post('http://localhost:8000/api/messages', {
+      username: this.username,
+      message: this.message
+    }).subscribe(() => this.message = '');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule} from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { RouterModule, Routes } from '@angular/router';
-
-
-const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-];
+import {AppComponent} from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent,
-    
-    
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(routes)
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
